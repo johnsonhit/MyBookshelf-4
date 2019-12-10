be606a00c725c022e8106612d6d521ce3d712cdf
Lee Jae Kyung <ninjajaekyung@gmail.com>
2019-12-10 13:17:23 +0900

    Initial Commit

---
+++ MyBookshelf/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyBookshelf
+//
+//  Created by Jae Kyung Lee on 10/12/2019.
+//  Copyright © 2019 Jae Kyung Lee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyBookshelfTests/MyBookshelfTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyBookshelfTests.swift
+//  MyBookshelfTests
+//
+//  Created by Jae Kyung Lee on 10/12/2019.
+//  Copyright © 2019 Jae Kyung Lee. All rights reserved.
+//
+
+import XCTest
+@testable import MyBookshelf
+
+class MyBookshelfTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
